fd9ae7656851cbf392b20aaa3174461a4d77cd7a
Dimitar Zoin <dimitar.zoin@gmail.com>
2017-07-28 23:37:44 +0300

    Added file and console logging

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
+"use strict";
+
 var express = require('express');
 var mongoose   = require('mongoose');
 var bodyParser = require('body-parser');
+const winston = require('winston');
 
 var Kitten = require('./Models/kitten');
+
+var logger = new (winston.Logger)({
+    transports: [
+      new (winston.transports.Console)({ level: 'info' }),
+      new (winston.transports.File)({
+        filename: './Logging/general.log',
+        level: 'info' //should be set through the ENV
+      })
+    ]
+  });
 
 var app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+//Routing
 var port = process.env.PORT || 8080;
 var router = express.Router();
 
@@ -20,4 +34,6 @@ app.use('/api', router);
 
 mongoose.connect('mongodb://localhost:27017');
 app.listen(port);
-console.log('Magic happens on port ' + port);
+
+// Test logging
+logger.info(`Magic happens on port ${port}`);
