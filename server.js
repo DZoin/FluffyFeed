623c1960bedbe498b46217fe21fbd89d0946ea91
Dimitar Zoin <dimitar.zoin@gmail.com>
2017-07-28 23:16:57 +0300

    Basic Kitten model

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require('express');
-var app = express();
+var mongoose   = require('mongoose');
 var bodyParser = require('body-parser');
+
+var Kitten = require('./Models/kitten');
+
+var app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -14,5 +18,6 @@ router.get('/', function(req, res) {
 
 app.use('/api', router);
 
+mongoose.connect('mongodb://localhost:27017');
 app.listen(port);
 console.log('Magic happens on port ' + port);
