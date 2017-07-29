d3d4fed63191323064e984c482999d1824909bb5
Dimitar Zoin <dimitar.zoin@gmail.com>
2017-07-29 13:07:01 +0300

    Added express-winston to support automatic logging of requests and responses while developing

--- server.js
+++ server.js
@@ -4,24 +4,43 @@ const express = require('express');
 const mongoose   = require('mongoose');
 const bodyParser = require('body-parser');
 const winston = require('winston');
+const expressWinston = require('express-winston');
 const config = require('./config.json');
 
 const Kitten = require('./Models/kitten');
 
+// TO DO: Logs must output structured, pretty printed JSON literals.
+// JSON.stringify() outpus too complex json with mongoose objects (override toString?)
 const logger = new (winston.Logger)({
     transports: [
-      new (winston.transports.Console)({ level: 'info' }),
+      new (winston.transports.Console)({ 
+        level: 'info',
+        json: false,
+        colorize: true
+    }),
       new (winston.transports.File)({
         filename: './Logging/general.log',
-        level: 'info' //should be set through the ENV
+        level: 'info', //should be set through the ENV
+        json: false
       })
     ]
   });
 
 const app = express();
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(expressWinston.logger({
+    transports: [
+        new winston.transports.Console({
+          json: true,
+          colorize: true
+        })
+      ],
+      meta: true, // optional: control whether you want to log the meta data about the request (default to true)
+      msg: "HTTP {{req.method}} {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
+      expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
+      colorize: true
+}));
 
 //Routing
 const port = process.env.PORT || 8080;
@@ -46,7 +65,6 @@ router.route('/kittens')
             if (err) {
                 res.send(err);
             }
-            
             res.json({ message: 'Kitten created!' });
         });
     })
@@ -54,7 +72,6 @@ router.route('/kittens')
         Kitten.find(function(err, kittens) {
             if (err)
                 res.send(err);
-
             res.json(kittens);
         });
     });
