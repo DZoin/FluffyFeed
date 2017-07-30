d964e54556bb1939d67c6736c59e0fadd0be338f
Dimitar Zoin <dimitar.zoin@gmail.com>
2017-07-30 15:31:35 +0300

    Extracted uuid generator into seperate file

--- Token/tokenRouter.js
+++ Token/tokenRouter.js
@@ -5,20 +5,10 @@ const jwt = require('jsonwebtoken');
 const logger = require('../logger.js');
 const jwtconfig = require('../Config/jwtconfig.json');
 const Token = require('./token.js');
+const generateUUID = require('../Utils/uuid.js');
 const router = express.Router();
 const one_day = 1440;
 
-function generateUUID() { // Public Domain/MIT
-    var d = new Date().getTime();
-    if (typeof performance !== 'undefined' && typeof performance.now === 'function') {
-        d += performance.now(); //use high-precision timer if available
-    }
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-        var r = (d + Math.random() * 16) % 16 | 0;
-        d = Math.floor(d / 16);
-        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
-    });
-}
 /**
  * @api {get} /token Get a JWT
  * @apiName GetToken
