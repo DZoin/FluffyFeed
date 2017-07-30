928486f481dd0d651105c689d57537543c66ed2f
Dimitar Zoin <dimitar.zoin@gmail.com>
2017-07-30 17:11:10 +0300

    Added sanitization for image_uri so the path provided cannot change directories

--- Image/imageRouter.js
+++ Image/imageRouter.js
@@ -12,6 +12,9 @@ const router = express.Router();
  * @apiParam{string}image_uri The uri to the thumbnail. Retrieved from the corresponding Kitten object
  */
 router.get("/:image_uri", function (req, res) {
+    if(req.params.image_uri.includes("/")) {
+        res.status(bad_request).send("Error! Badly formed image uri");
+    }
     const html = `<img src="/${req.params.image_uri}?token=${req.query.token}" />`
 
     res.send(html);
