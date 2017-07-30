e3c569ed81c9cb817b3dfceb60a7513475984df5
Dimitar Zoin <dimitar.zoin@gmail.com>
2017-07-30 10:51:48 +0300

    Changed request parameters from querystring to url

--- Kittens/kittenRouter.js
+++ Kittens/kittenRouter.js
@@ -8,71 +8,78 @@ const Kitten = require('./kitten.js');
 const logger = require('../logger.js');
 const internal_server_error = 500;
 
-router.route("")
-    /**
-     * @api {post} /kitten Create a kitten profile
-     * @apiName PostKitten
-     * @apiGroup Kitten
-     */
-    .post(function (req, res) {
-        let kitten = new Kitten();
-        kitten.name = req.query.name;
+/**
+ * @api {post} /kitten/:name Create a kitten profile
+ * @apiName PostKitten
+ * @apiGroup Kitten
+ */
+router.post("/:name", function (req, res) {
+    let kitten = new Kitten();
+    kitten.name = req.params.name;
 
-        kitten.save(function (err) {
-            if (err) {
-                logger.error(`Query failed with error: ${err}`)
-                res.status(internal_server_error).send("Error! Kitten creation failed!");
-            }
-            res.json({ message: 'Kitten created!' });
-        });
-    })
-    /**
-     * @api {get} /kitten Request kitten feed
-     * @apiName GetKitten
-     * @apiGroup Kitten
-     */
-    .get(function (req, res) {
-        const pageSize = parseInt(req.query.pageSize) || 5;
-
-        const last_comment_id = ObjectId.isValid(req.query.index) ? new ObjectId(req.query.index) : null;
-
-        let kittenQuery = null;
-
-        if (last_comment_id) {
-            kittenQuery = Kitten.find()
-                .where("_id").gt(last_comment_id)
-                .limit(pageSize)
-                .exec();
-
-        } else {
-            kittenQuery = Kitten.find().limit(pageSize).exec();
-        }
-
-        kittenQuery.then((kittens) => {
-            const responseBody = {
-                "kittens": kittens,
-                "pageSize": pageSize
-            }
-            if (kittens.length == pageSize) {
-                responseBody.index = kittens[kittens.length - 1]._id
-            }
-            res.json(responseBody);
-        }).catch(err => {
+    kitten.save(function (err) {
+        if (err) {
             logger.error(`Query failed with error: ${err}`)
-            res.status(internal_server_error).send("Error! Kitten retrieval failed!");
-        });
+            res.status(internal_server_error).send("Error! Kitten creation failed!");
+        }
+        res.json({ message: 'Kitten created!' });
     });
+});
+/**
+ * @api {get} /kitten/:pagesize?/:index? Request kitten feed
+ * @apiName GetKitten
+ * @apiGroup Kitten
+ * @apiSuccess{Array}kittens Array of Kitten objects
+ * @apiSuccess{int}pageSize Current pagination size
+ * @apiSuccess{string}index [Optional]Index of last item. If not present end of feed has been reached
+ * @apiSuccessExample Sample response
+  {
+    "kittens": [
+        {
+            "_id": "597c2c02f9ec57078e333e5a",
+            "name": "MrFluff",
+            "__v": 0
+        },
+        {
+            "_id": "597c3448d5e87d1845e02dea",
+            "name": "Stoyancho",
+            "__v": 0
+        }
+    ],
+    "pageSize": 2,
+    "index": "597d75cc67e0f246367ca827"
+    }
+ */
+router.get("/:pagesize?/:index?", function (req, res) {
+    const pageSize = parseInt(req.params.pagesize) || 5;
 
+    const last_comment_id = ObjectId.isValid(req.params.index) ? new ObjectId(req.params.index) : null;
 
-router.get("/:kitten_name_filter", function (req, res) {
-    const regex = new RegExp(`.*${req.params.kitten_name_filter}.*`, "i"); // TODO: refactor with query builder
-    var kittenQuery = Kitten.find({ name: regex }).exec();
+    let kittenQuery = null;
 
-    kittenQuery.then((err, kittens) => {
-        res.json(kittens);
+    if (last_comment_id) {
+        kittenQuery = Kitten.find()
+            .where("_id").gt(last_comment_id)
+            .limit(pageSize)
+            .exec();
+
+    } else {
+        kittenQuery = Kitten.find().limit(pageSize).exec();
+    }
+
+    kittenQuery.then((kittens) => {
+        const responseBody = {
+            "kittens": kittens,
+            "pageSize": pageSize
+        }
+        if (kittens.length == pageSize) {
+            responseBody.index = kittens[kittens.length - 1]._id
+        }
+        res.json(responseBody);
     }).catch(err => {
         logger.error(`Query failed with error: ${err}`)
         res.status(internal_server_error).send("Error! Kitten retrieval failed!");
-    });;
+    });
 });
+
 module.exports = router;
--- Token/tokenRouter.js
+++ Token/tokenRouter.js
@@ -8,9 +8,9 @@ const Token = require('./token.js');
 const router = express.Router();
 const one_day = 1440;
 
-function generateUUID () { // Public Domain/MIT
+function generateUUID() { // Public Domain/MIT
     var d = new Date().getTime();
-    if (typeof performance !== 'undefined' && typeof performance.now === 'function'){
+    if (typeof performance !== 'undefined' && typeof performance.now === 'function') {
         d += performance.now(); //use high-precision timer if available
     }
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
@@ -20,34 +20,33 @@ function generateUUID () { // Public Domain/MIT
     });
 }
 
-router.route("")
-    .get(function(req, res) {
-        const token = jwt.sign({ id: generateUUID()}, jwtconfig.secret, {
-          expiresIn: one_day,
-          algorithm: jwtconfig.algorithm
-        });
-
-        res.json({
-          success: true,
-          message: 'Token issued successfully!',
-          token: token
-        });
-    })
-    .delete(function(req, res) {
-        const token = req.query.token;
-
-        const decodedToken = jwt.verify(req.query.token, jwtconfig.secret)
-        
-        const tokenObj = new Token();
-        tokenObj.id = decodedToken.id;
-        
-        tokenObj.save(function(err) {
-            if (err) {
-                logger.error(`Token blacklist failed with error: ${err}`);
-                res.status(500).send("Session invalidation failed!");
-            }
-            res.json({ message: 'Session successfully invalidated!' });
-        });
+router.get(function (req, res) {
+    const token = jwt.sign({ id: generateUUID() }, jwtconfig.secret, {
+        expiresIn: one_day,
+        algorithm: jwtconfig.algorithm
     });
-    
+
+    res.json({
+        success: true,
+        message: 'Token issued successfully!',
+        token: token
+    });
+});
+router.delete("/:token", function (req, res) {
+    const token = req.params.token;
+
+    const decodedToken = jwt.verify(req.query.token, jwtconfig.secret)
+
+    const tokenObj = new Token();
+    tokenObj.id = decodedToken.id;
+
+    tokenObj.save(function (err) {
+        if (err) {
+            logger.error(`Token blacklist failed with error: ${err}`);
+            res.status(500).send("Session invalidation failed!");
+        }
+        res.json({ message: 'Session successfully invalidated!' });
+    });
+});
+
 module.exports = router;
